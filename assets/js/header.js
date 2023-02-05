1d3840331da8ae303d3b8d0fae8c1e0204de8156
createyamada <97172703+createyamada@users.noreply.github.com>
2023-02-05 17:06:30 +0900

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -23,3 +23,38 @@ resCom.onclick = function (e) {
     console.log("メニュ要素クリック",e);
     onHeaderMenuClick('close');
 }
+
+
+window.addEventListener('scroll',function(){
+	const scrollTop = document.documentElement.scrollTop; // スクロール上部の位置
+    const whiteArea = document.getElementsByClassName("whiteArea");
+    const WhiteAreaRect = whiteArea[0].getBoundingClientRect();
+    const areaTop  = WhiteAreaRect.top + scrollY;
+    console.log(scrollTop)
+    console.log(areaTop)
+
+	if (scrollTop > areaTop ) {
+        console.log("whiteクラスに入りました")
+        // 画像要素を取得
+        var white = document.getElementById("whiteLogo");
+        var black = document.getElementById("blackLogo");
+
+        // クラス名を追加削除
+        black.classList.remove("nonDisp");
+        white.classList.remove("nonDisp");
+
+        white.classList.add("nonDisp");
+
+	} else {
+		console.log("blackクラスです")
+        // 画像要素を取得
+        var white = document.getElementById("whiteLogo");
+        var black = document.getElementById("blackLogo");
+
+        // クラス名を追加削除
+        white.classList.remove("nonDisp");
+        black.classList.remove("nonDisp");
+
+        black.classList.add("nonDisp");
+	}
+  })
